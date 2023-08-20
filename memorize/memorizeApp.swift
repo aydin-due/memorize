37841ef1afaa16f9337450f708e4f29c051ff5a5
aydin <aydindue.tech@gmail.com>
2023-08-20 12:56:42 -0700

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by aydin salman on 20/08/23.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
